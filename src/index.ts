cfe965017e6cee6def0d3ab48aa15d4edf1a907a
Daniel Brada <danik.brada@seznam.cz>
2020-05-17 09:00:02 +0200

    typo

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = Express();
 app.use(Express.json())
 
 app.post("/webhooks/recordone", (req, res)=>{
-	res.set("Access-Controll-Allow-Origin", "*")
+	res.set("Access-Control-Allow-Origin", "*")
 	console.log("Request coming!")
 	console.log(req)
 	console.log(req.body)
